0d2bb5a552114b69db588175edd3e55303f029be
venuatu <venuatu@gmail.com>
2023-03-05 18:45:27 +1100

    example: use pyarrow memory mapped tensors for faster loading

--- example.py
+++ example.py
@@ -8,6 +8,7 @@ import torch
 import fire
 import time
 import json
+import pyarrow as pa
 
 from pathlib import Path
 
@@ -21,18 +22,42 @@ def load(
     max_batch_size: int,
 ) -> LLaMA:
     start_time = time.time()
-    checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
-    # TODO: support larger models
-    ckpt_path = checkpoints[0]
+    arrow_dir = Path(ckpt_dir).expanduser() / 'arrow'
+
+    if not arrow_dir.exists():
+        print('Converting checkpoints to arrow format')
+        checkpoints = sorted(Path(ckpt_dir).expanduser().glob("*.pth"))
+        for ckpt_file in checkpoints:
+            print(ckpt_file)
+            index = ckpt_file.parts[-1].split('.')[-2]
+
+            ckpt = torch.load(ckpt_file, map_location='cpu')
+            (arrow_dir / index).mkdir(parents=True, exist_ok=True)
+            for k, v in ckpt.items():
+                tens = pa.Tensor.from_numpy(v.numpy())
+                with pa.output_stream(arrow_dir / index / k) as f:
+                    pa.ipc.write_tensor(tens, f)
+            ckpt = None
+
+    with open(Path(ckpt_dir) / "params.json", "r") as f:
+        params = json.loads(f.read())
+
+    print("Loading checkpoint")
+    segments = sorted((arrow_dir / '00').glob("*"))
+    # print(segments)
+
+    checkpoint = {}
+    files = []
+    for seg in segments:
+        f = pa.memory_map(str(seg))
+        files.append(f)
+        t = pa.ipc.read_tensor(f).to_numpy()
+        t = torch.from_numpy(t)
+        checkpoint[seg.parts[-1]] = t
 
     # torch.set_default_tensor_type(torch.cuda.HalfTensor)
     torch.set_default_tensor_type(torch.BFloat16Tensor)
     # torch.set_default_tensor_type(torch.FloatTensor)
-
-    print("Loading checkpoint")
-    checkpoint = torch.load(ckpt_path, map_location="cpu")
-    with open(Path(ckpt_dir) / "params.json", "r") as f:
-        params = json.loads(f.read())
 
     model_args: ModelArgs = ModelArgs(
         max_seq_len=max_seq_len, max_batch_size=max_batch_size, **params
@@ -43,6 +68,10 @@ def load(
     print("Loading model")
     model = Transformer(model_args)
     model.load_state_dict(checkpoint, strict=False)
+
+    for f in files:
+        f.close()
+    files = None
 
     generator = LLaMA(model, tokenizer)
     print(f"Loaded in {time.time() - start_time:.2f} seconds")
